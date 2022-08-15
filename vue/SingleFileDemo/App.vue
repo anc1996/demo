cc07581e8f66da30f69a6ced402186135a233261
anc1996 <39521540+anc1996@users.noreply.github.com>
2022-08-15 18:00:57 +0800

    单文件组件css代码用法

--- vue/SingleFileDemo/App.vue
+++ vue/SingleFileDemo/App.vue
@@ -6,10 +6,20 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  data:function (){
+    return {
+      name:'python'
+    }
+  }
 }
 </script>
-
+<!--样式中如果有scope关键字，表示这些样式是组件局部的，不会影响其他元素-->
 <style scoped>
 
+  div{
+    color:red;
+    font-weight:bold;
+    text-indent:10px;
+  }
 </style>
